7e4236c72d691f8f62bdb7b5f012f0a7071896a5
hari-44456 <naraharipapshetwar@gmail.com>
2021-11-08 14:45:59 +0530

    Adds.Added provider file at runtime

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def view_home():
 
 @app.route("/aws")
 def aws():
-	lines = json.loads(open("aws_images.json").read())
+	lines = json.loads(open("data/aws_images.json").read())
 	return render_template("aws.html", title="Aws",opt=lines)   
 
 
@@ -24,46 +24,79 @@ def generateApplyCommand(pairs,st="apply"):
 
 @app.route("/aws", methods=['POST'])
 def aws_post():
+	directory = "aws"
 
 	input_data = eval(request.form.getlist('ami')[0])
+	region = input_data.get("region")
+
+	if re.search("windows",input_data["os_name"]):
+		directory = "aws-win"
 	
 	pairs={} 
 	for key,v in input_data.items():
+		if key == 'os_name':
+			continue
 		pairs[key] = v
 
-	file = request.files['file']
-	filename = secure_filename(file.filename) 
-	file.save(filename)
+	# configures providers.tf	
+	if len(request.form.getlist("AlreadyConfigured")) == 0:
+		file = request.files['file']
+		filename = secure_filename(file.filename) 
+		file.save(filename)
 
-	lines =[]
-	with open(filename) as f:
-		lines = f.readlines()
+		lines =[]
+		with open(filename) as f:
+			lines = f.readlines()
 
-	access_key=None 
-	secret_key=None
+		access_key=None 
+		secret_key=None
 
-	for line in lines:
-		tmp = line.replace(' ','')[:-1]
-		if re.search("access",line) and re.search("key",line) and re.search("secret",line) == None:
-			result = tmp.find('=')
-			access_key=tmp[result+1:]
-		if re.search("key",line) and re.search("secret",line):
-			result = tmp.find('=')
-			secret_key=tmp[result+1:]
+		for line in lines:
+			tmp = line.replace(' ','')[:-1]
+			if re.search("access",line) and re.search("key",line) and re.search("secret",line) == None:
+				result = tmp.find('=')
+				access_key=tmp[result+1:]
+			if re.search("key",line) and re.search("secret",line):
+				result = tmp.find('=')
+				secret_key=tmp[result+1:]
 
-	if access_key == None or secret_key == None:
-		return render_template("aws.html",title="Aws")
-	
+		if access_key == None or secret_key == None:
+			print("Unable to configure aws keys")
+			return render_template("aws.html",title="Aws")
+		
+		content = '''
+			provider "aws" {{
+				access_key = "{access_key}"
+				secret_key = "{secret_key}"
+				region = "{region}"
+			}}
+		'''
+		print(content.format(access_key=access_key,secret_key=secret_key,region=region))
+		os.chdir(directory)
+		provider_file = open("providers.tf", "w")
+		provider_file.write(content.format(access_key=access_key,secret_key=secret_key,region=region))
+		provider_file.close()
+		os.chdir("..")
+		os.remove(filename)
+	else:
+		content = '''
+			provider "aws" {}
+		'''
+		print(content)
+		os.chdir(directory)
+		provider_file = open("providers.tf", "w")
+		provider_file.write(content)
+		provider_file.close()
+		os.chdir("..")
+		
 	cmd=generateApplyCommand(pairs)
 	print(cmd)
 	
-	# os.chdir("aws")
-	
-	# os.system("terraform init -upgrade")
-	# os.system (cmd)
-	#os.system("terraform state rm \"aws_ami_from_instance.ami\" ")
-	#destory =generateApplyCommand(pairs,"destory")
-	#os.system(destory)
+	os.chdir(directory)
+	os.system("terraform init -upgrade")
+	os.system (cmd)
+	os.system("terraform state rm \"aws_ami_from_instance.ami\" ")
+	os.system(generateApplyCommand(pairs,"destory"))
 	return render_template("aws.html", title="Aws")
 
 @app.route("/azure",methods=['GET'])
@@ -174,33 +207,6 @@ def gcp_post():
 	os.system(cmd)
 	os.chdir("..")
 	return render_template("gcp.html",title="gcp")
-
-
-@app.route('/getfile', methods=['GET','POST'])
-def getfile():
-    if request.method == 'POST':
-
-        # for secure filenames. Read the documentation.
-        file = request.files['file']
-        filename = secure_filename(file.filename) 
-
-        # os.path.join is used so that paths work in every operating system
-        file.save(filename)
-        lines = []
-        with open(filename) as f:
-        	lines = f.readlines()
-        count = 0
-        for line in lines:
-        	result = line.find('=')
-        	print(line[result+1:])      
-
-
-    else:
-        result = request.args.get['myfile']
-    return result
-
-
-
 
 
 if __name__ == '__main__':
